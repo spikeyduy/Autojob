4aa731d84910d883aa3ecdfdc08c350e3e6ca1f7
Spikes Phan <spikeyduy@gmail.com>
2017-01-25 17:04:53 -0600

    Used Ctypes to emulate the key presses.

--- ShootingRange.py
+++ ShootingRange.py
@@ -1,3 +1,42 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://seleniumhq.org/')
+from bs4 import BeautifulSoup
+import requests
+import ctypes, time
+
+# browser = webdriver.Firefox()
+# browser.get('http://seleniumhq.org/')
+
+# res = requests.get('http://mysoft.healthcare.uiowa.edu/')
+# # res.raise_for_status()
+# testSoup = BeautifulSoup(res.text, 'html.parser')
+# # print(testSoup)
+# inField = testSoup.select('input')
+# for i in range(0,len(inField)):
+#     print(str(inField[i]))
+
+ctypes.windll.user32.keybd_event(0x12, 0, 0, 0)
+ctypes.windll.user32.keybd_event(0x09, 0, 0, 0)
+
+time.sleep(2)
+
+ctypes.windll.user32.keybd_event(0x09, 0, 2, 0)
+ctypes.windll.user32.keybd_event(0x012, 0, 2, 0)
+
+time.sleep(2)
+
+ctypes.windll.user32.keybd_event(0xA2, 0, 0, 0)
+ctypes.windll.user32.keybd_event(0x56, 0, 0, 0)
+ctypes.windll.user32.keybd_event(0xA2, 0, 2, 0)
+ctypes.windll.user32.keybd_event(0x56, 0, 2, 0)
+time.sleep(1)
+ctypes.windll.user32.keybd_event(0x0D, 0, 0, 0)
+ctypes.windll.user32.keybd_event(0x0D, 0, 2, 0)
+time.sleep(2)
+ctypes.windll.user32.keybd_event(0x09, 0, 0, 0)
+ctypes.windll.user32.keybd_event(0x09, 0, 2, 0)
+# ctypes.windll.user32.keybd_event(0x09, 0, 0, 0)
+# ctypes.windll.user32.keybd_event(0x09, 0, 2, 0)
+time.sleep(0.2)
+ctypes.windll.user32.keybd_event(0x0D, 0, 0, 0)
+ctypes.windll.user32.keybd_event(0x0D, 0, 2, 0)
+
--- autojob.py
+++ autojob.py
@@ -6,10 +6,21 @@
 # Tab ? times and input the custom date. Tab to save and complete, loop.
 import openpyxl
 import os
+from bs4 import BeautifulSoup
 import urllib
+import requests
+import ctypes, time
 
 class autoJob():
     def __init__(self):
+        ctypes.windll.user32.keybd_event(0x12, 0, 0, 0) # alt
+        ctypes.windll.user32.keybd_event(0x09, 0, 0, 0) # tab
+
+        time.sleep(2) # wait 2
+
+        ctypes.windll.user32.keybd_event(0x09, 0, 2, 0) # !alt
+        ctypes.windll.user32.keybd_event(0x012, 0, 2, 0) # !tab
+
         self.getfile()
 
     # function to copy a variable to the clipboard
@@ -21,15 +32,45 @@ class autoJob():
     # This does all the leg work
     def humanJob(self, serial):
         # print(serial)
+        # MAY NOT NEED THIS
+        # could do with just letting it sit in the variable form
+        # and just input it into the text box
         self.addToClip(serial)
-        print(type(serial), serial)
+        ctypes.windll.user32.keybd_event(0xA2, 0, 0, 0) # ctrl
+        ctypes.windll.user32.keybd_event(0x56, 0, 0, 0) # v
+        ctypes.windll.user32.keybd_event(0xA2, 0, 2, 0) # !ctrl
+        ctypes.windll.user32.keybd_event(0x56, 0, 2, 0) # !v
+        ctypes.windll.user32.keybd_event(0xA2, 0, 2, 0)
+        ctypes.windll.user32.keybd_event(0x56, 0, 2, 0)
+        time.sleep(1)
+        ctypes.windll.user32.keybd_event(0x0D, 0, 0, 0) # enter
+        ctypes.windll.user32.keybd_event(0x0D, 0, 2, 0)
+        time.sleep(1)
+        ctypes.windll.user32.keybd_event(0x09, 0, 0, 0)  # tab
+        ctypes.windll.user32.keybd_event(0x09, 0, 2, 0)
+        ctypes.windll.user32.keybd_event(0x09, 0, 0, 0)  # tab
+        ctypes.windll.user32.keybd_event(0x09, 0, 2, 0)
+        ctypes.windll.user32.keybd_event(0x0D, 0, 0, 0)  # enter
+        ctypes.windll.user32.keybd_event(0x0D, 0, 2, 0)
+        time.sleep(1)
+        for i in range(29):
+            print('tabbing through')
+            ctypes.windll.user32.keybd_event(0x09, 0, 0, 0)  # tab
+            ctypes.windll.user32.keybd_event(0x09, 0, 2, 0)
+            time.sleep(0.2)
+        ctypes.windll.user32.keybd_event(0x0D, 0, 0, 0)  # enter
+        ctypes.windll.user32.keybd_event(0x0D, 0, 2, 0)
+        time.sleep(3)
+        # print(type(serial), serial)
+        # alt tab over
 
     def getfile(self):
-        filePath = raw_input("Please enter the full path of the spreadsheet:")
+        # filePath = raw_input("Please enter the full path of the spreadsheet:")
         # print(filePath)
         # returnDate = input("Please enter the date in mm/dd/yyyy format for day"
         #                    " of trade in: ")
-        wb = openpyxl.load_workbook(filePath)
+        # wb = openpyxl.load_workbook(filePath)
+        wb = openpyxl.load_workbook('test.xlsx')
         # print(type(wb))
 
         # fetch the sheets of the workbook
